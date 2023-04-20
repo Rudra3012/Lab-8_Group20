9deccc2d81f58ad820887c030e9cecca51b8b0a3
Rudra Gohel <202001178@daiict.ac.in>
2023-04-20 21:12:04 +0530

    -F

--- test_.py
+++ test_.py
@@ -9,7 +9,19 @@ def test_login1():
     response = LoginPage({'username': 'Ruchir17', 'pass': 'ruchir123'})
     assert response['message'] == 'Login Unsuccessful'
 
-def test_login():
+def test_login2():
     response = LoginPage({'username': 'Ruchir17', 'pass': 'Ruchir123'})
     assert response['message'] == 'Login Unsuccessful'
+
+def test_login3():
+    response = LoginPage({'username': 'Ruchir17', 'pass': 'Ru@123'})
+    assert response['message'] == 'Login Unsuccessful'
+
+def test_login4():
+    response = LoginPage({'username': 'Ruchir17', 'pass': 'Ruchir12345678998948974987984'})
+    assert response['message'] == 'Login Unsuccessful'
+
+def test_login5():
+    response = LoginPage({'username': 'Ruchir17', 'pass': 'Ruchir@123'})
+    assert response['message'] == 'Login Successful'
 
